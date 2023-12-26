8eb68806946def4a84d9cf46646fd95976e32e8f
fgsfds <derp.primus@gmail.com>
2023-12-26 23:15:05 +0100

    port: net: fix linux builds probably

--- port/external/enet.c
+++ port/external/enet.c
@@ -1,3 +1,10 @@
+#ifndef _WIN32
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#endif
+#endif
+
 #define ENET_IMPLEMENTATION 1
 #define ENET_NO_PRAGMA_LINK 1
+
 #include "external/enet.h"
